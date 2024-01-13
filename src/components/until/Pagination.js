b7c01fdb03561876b4f18f700d1a9232b2dfcaf9
TuanDoan <139220269+TuanDQ7-fpt@users.noreply.github.com>
2024-01-13 23:52:24 +0700

    Update paging

--- src/components/until/Pagination.js
+++ src/components/until/Pagination.js
@@ -1,35 +1,32 @@
-import * as React from 'react';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import * as React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 
 export default function PaginationRounded(props) {
   return (
-    <div className='pagination-class'>
-    
-    <span>表示件数：</span>
+    <div className="pagination-class">
+      <span>表示件数：</span>
       <Select
         value={props.pageSize}
         onChange={props.handleChangePageSize}
-        className='select-option'
+        className="select-option"
       >
-        <MenuItem value={25}>25</MenuItem>
-        <MenuItem value={35}>35</MenuItem>
-        <MenuItem value={50}>50</MenuItem>
+        <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={30}>30</MenuItem>
+        <MenuItem value={50}>30</MenuItem>
       </Select>
-      
 
-    <Stack spacing={2}>
-      <Pagination count={props.totalCount} 
-        shape="rounded"
-        onChange={props.handleChangePage}
-        color="primary"
-        page={props.currentPage}
-      />
-    </Stack>
+      <Stack spacing={2}>
+        <Pagination
+          count={props.totalCount}
+          shape="rounded"
+          onChange={props.handleChangePage}
+          color="primary"
+          page={props.currentPage}
+        />
+      </Stack>
     </div>
   );
 }
